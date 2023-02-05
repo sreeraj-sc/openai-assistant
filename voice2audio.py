9973d7599a619fcc232f986a6a5d8361a9eccf55
sreeraj-sc <sreeraj.s.c.vfc@gmail.com>
2023-02-05 12:50:42 +0530

    it can now take voice input convert it to text and get responce for openai and speck to it as

--- voice2audio.py
+++ voice2audio.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
 from gtts import gTTS
 import os
+import openai
+
+# Set up OpenAI API client
+openai.api_key = "YOUR_API_KEY"
 
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
@@ -15,11 +19,24 @@ with sr.Microphone() as source:
 # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
 try:
     # using google speech recognition
-    print("Text: "+r.recognize_google(audio_text))
-    response_text = r.recognize_google(audio_text)
+    print("You said: "+r.recognize_google(audio_text))
+    input_text = r.recognize_google(audio_text)
 except:
      print("Sorry, I did not get that")
-     response_text = "Sorry, I did not get that"
+     input_text = "Sorry, I did not get that"
+
+# Use OpenAI API to generate a response to the input text
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=input_text,
+  max_tokens=1024,
+  n=1,
+  stop=None,
+  temperature=0.5,
+)
+
+# Get the generated response text
+response_text = response["choices"][0]["text"].strip()
 
 # Generate speech from the response text
 tts = gTTS(response_text)
